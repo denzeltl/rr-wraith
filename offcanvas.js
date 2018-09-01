49a7a691c95d7796c8546e5bba461ec935bf9dfb
Denzel <denzel706@yahoo.com.ph>
2018-09-02 00:00:47 +0800

    Project Completed

--- offcanvas.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("burger-menu");
-    if (x.className === "navbar-start") {
-        x.className += " responsive";
-    } else {
-        x.className = "navbar-start";
-    }
-}
